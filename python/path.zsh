2d488cbff1b5b579ab387cb273cc2b054f07f222
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2024-02-04 12:38:35 -0500

    gimmie python

--- python/path.zsh
+++ python/path.zsh
@@ -1,1 +1,8 @@
 export PATH="$HOME/.poetry/bin:$PATH"
+
+export PYENV_ROOT="$HOME/.pyenv"
+
+if [[ -d "$PYENV_ROOT" ]]; then
+    # Add Pyenv to the path
+    export PATH="$PYENV_ROOT/bin:$PATH"
+fi
--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,1 +1,20 @@
-eval "$(pyenv init -)"
+#! /usr/bin/env bash
+
+# Disable stdout/stderr buffering, equivalent to -u
+export PYTHONUNBUFFERED=true
+
+# Don't write .pyc files, equivalent to -B
+export PYTHONDONTWRITEBYTECODE=true
+
+# Require a python virtual environment for pip to install things
+# export PIP_REQUIRE_VIRTUALENV=true
+
+# Custom global pip install
+function gpip() {
+    PIP_REQUIRE_VIRTUALENV="" pip "$@"
+}
+
+if [[ -d "$PYENV_ROOT" ]]; then
+    # Initialize Pyenv in the shell
+    eval "$(pyenv init -)"
+fi
